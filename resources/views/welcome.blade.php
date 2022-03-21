fbea04a3278baa72e25b3dfbc3544c2a333a37a2
DICACHALO <dchacon871@unab.edu.co>
2022-03-20 23:48:02 -0500

    style search

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach($posts as $post)
-    <div class="container">
+<section class="content container-fluid">
+    <div class="container m-1 ">
+        <div class="card ">
+            <div class="card-body ">
+            <form method="POST" action="" >
+                @csrf
+                    <label class="p-1 ">Selecciona una fecha inicial:</label>
+                    <input name="date_ini" value="date" type="date"  id="date_ini" >
+                    <label class="p-1">Selecciona una fecha final:</label>
+                    <input name="date_finish" value="date" type="date"  id="date_finish" >&nbsp;&nbsp;&nbsp;&nbsp;
+                    <button type="submit" class="btn btn-warning p-1" target="_blank">&nbsp;&nbsp;
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search-heart" viewBox="0 0 16 16">
+                    <path d="M6.5 4.482c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.69 0-5.018Z"/>
+                    <path d="M13 6.5a6.471 6.471 0 0 1-1.258 3.844c.04.03.078.062.115.098l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1.007 1.007 0 0 1-.1-.115h.002A6.5 6.5 0 1 1 13 6.5ZM6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11Z"/>
+                    </svg>&nbsp;&nbsp; Buscar posts &nbsp;&nbsp;</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+ 
+@foreach($posts as $post)
     <section class="content container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card m-2 ">
+    <div class="container m-1">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card">
                         <div class="card-header bg-primary text-white">
                             <div class="float-left">
                                 <span class="card-title"> <h4>{{$post->title}}</h4></span>
